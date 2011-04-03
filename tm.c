447eac305766c0f4837032c4d3440530f7e76843
Samuel Christie <emailium@gmail.com>
2011-04-03 16:42:43 -0400

    Add tm.c

---
+++ tm.c
@@ -0,0 +1,92 @@
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+<head>
+<title>Login Required</title>
+<link rev="made" href="mailto:help%40ncsu.edu" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body link="#FF0000" vlink="#AA0000" topmargin="0" marginwidth="0" leftmargin="0" bgcolor="#FFFFFF" text="#555555" onload="document.affil.login.focus(); document.affil.login.select();" marginheight="0">
+
+<!--begin oit-nav-bar-->
+<link rel="stylesheet" type="text/css" href="https://ssl.ncsu.edu/oit_branding/nav_bar_iframe.css"> 
+<iframe name="OITNav" id="OITNav" title="OITNav" src="https://ssl.ncsu.edu/oit_branding/nav_bar.php?color=red" scrolling="no">
+Your browser does not support inline frames or is currently configured not to display inline frames. <br>
+Visit <a href="http://oit.ncsu.edu">http://oit.ncsu.edu</a>. 
+</iframe>
+<!--end oit-nav-bar-->
+
+
+<TABLE BORDER=0 CELLPADDING=8 CELLSPACING=8 WIDTH="100%">
+<TR><TD VALIGN=TOP>
+  <TABLE BORDER=0 CELLPADDING=8 CELLSPACING=4 WIDTH="100%">
+
+  <TR><TD VALIGN=TOP ALIGN=LEFT WIDTH="50%">
+  <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=6><B>Login Required</B></FONT><BR>
+  <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=2><I>Please choose an affiliation</I></FONT><BR>
+  <BR>
+  </TD><TD VALIGN=TOP ALIGN=RIGHT WIDTH="50%">
+  <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=6><B>
+  <A HREF="https://webauth.ncsu.edu/wrap-bin/was16.cgi?logout">Logout</A></B></FONT>
+  </TD></TR>
+
+  <TR><TD VALIGN=TOP ALIGN=LEFT WIDTH="50%">
+<!-- WAS::StartHTML ends -->
+
+<form method="post" action="https://webauth.ncsu.edu/wrap-bin/was16.cgi?gosite" enctype="application/x-www-form-urlencoded" name="affil">
+<BLOCKQUOTE>
+   <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=4><B>Affiliation Status:</B></FONT><BR>
+   <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=2>
+   Please indicate with whom you are affiliated:<BR><BR>
+
+<select name="WAS_SITE" >
+<option value="https://webauth.ncsu.edu/wrap-bin/was16.cgi?getuser">NC State University</option>
+<option value="https://www.lib.ncsu.edu/proxy/patronauth.html?wascom+https://webauth.ncsu.edu/wrap-bin/was16.cgi+152.7.1.242+1301857901">NCSU Libraries Patron</option>
+<option value="https://webauth.ncsu.edu/wrap-bin/was16.cgi?guest">Guest Access</option>
+</select><BR><BR>
+<input type="submit" name="login" value="Continue Login" />
+  </FONT>
+</BLOCKQUOTE>
+</form>
+<!-- WAS::MiddleHTML begins -->
+  </TD><TD VALIGN=TOP ALIGN=LEFT WIDTH="50%" BGCOLOR="#EEEEEE">
+  <FONT FACE="verdana, arial, helvetica, sans-serif" SIZE=2 COLOR="#000000">
+<!-- WAS::MiddleHTML ends -->
+
+<FONT SIZE=3><B>Does your browser accept cookies?</B></FONT>
+WRAP issues an encrypted cookie named WRAP16 that contains your 
+login credentials. Your browser must accept this cookie and pass it
+to other ncsu.edu webservers for WRAP to work correctly. 
+<BR><BR>
+
+<FONT SIZE=3><B>What do you need to use this?</B></FONT>
+WRAP requires you to login using your NC State Unity user ID and password.
+If you are not an NC State faculty, staff, or student, you can still
+login using our Guest account. Note, however, that this account has
+restricted rights.
+<BR><BR>
+
+<FONT SIZE=3><B>How do I log out?</B></FONT>
+You can remove your WRAP cookie by using the 
+<A HREF="https://webauth.ncsu.edu/wrap-bin/was16.cgi?logout">logout link</A>. Otherwise, 
+your cookie will automatically expire after a certain period of time,
+or your browser will remove the cookie when you close your session. 
+<BR><BR>
+
+<FONT SIZE=3><B>IE Users:</B></FONT>
+Due to a bug in IE, if after hitting the
+<B>Continue Login</B> button this page remains, please hit the
+<B>Continue Login</B> button again.
+
+<!-- WAS::EndHTML begins -->
+
+  </FONT>
+  </TD></TR>
+
+  </TABLE>
+</TD></TR></TABLE>
+
+
+</body>
+</html>
